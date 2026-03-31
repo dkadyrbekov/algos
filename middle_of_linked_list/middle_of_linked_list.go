8d3151dc322bed035ff8b2974695eab5eb380d46
d.kadyrbekov <d.kadyrbekov@tbank.ru>
2026-03-31 15:35:58 +0500

    middle of linked list fast and slow pattern

--- middle_of_linked_list/middle_of_linked_list.go
+++ middle_of_linked_list/middle_of_linked_list.go
@@ -1,1 +1,23 @@
 package middle_of_linked_list
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getMiddleNode(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	slow, fast := head, head
+
+	for {
+		if fast == nil || fast.Next == nil {
+			return slow
+		}
+
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+}
