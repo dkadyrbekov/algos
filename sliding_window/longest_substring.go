95b85ffb93c10d0fd73d02ba3081df523a00bd12
d.kadyrbekov <d.kadyrbekov@tbank.ru>
2026-04-01 14:50:51 +0500

    longest substring

--- sliding_window/longest_substring.go
+++ sliding_window/longest_substring.go
@@ -1,25 +1,22 @@
 package sliding_window
 
 func findLongestSubstring(str string) int {
-
-	strArr := []rune(str)
-
-	if len(strArr) == 0 {
+	if len(str) == 0 {
 		return 0
 	}
 
 	left, right := 0, 0
 	maxLen := 1
-	lettersMap := map[rune]int{
-		strArr[0]: 1,
+	lettersMap := map[byte]int{
+		str[0]: 1,
 	}
 
-	for right < len(strArr)-1 {
+	for right < len(str)-1 {
 		right++
-		lettersMap[strArr[right]]++
+		lettersMap[str[right]]++
 
-		for lettersMap[strArr[right]] > 1 {
-			lettersMap[strArr[left]]--
+		for lettersMap[str[right]] > 1 {
+			lettersMap[str[left]]--
 			left++
 		}
 
