06c20a637144902a6fe8ce1991acd45744ee3208
d.kadyrbekov <d.kadyrbekov@tbank.ru>
2026-04-15 22:46:00 +0500

    min cpus

--- heap/min_cpu.go
+++ heap/min_cpu.go
@@ -1,6 +1,9 @@
 package heap
 
-import "container/heap"
+import (
+	"container/heap"
+	"sort"
+)
 
 type MinIntHeap []int
 
@@ -20,35 +23,32 @@ func (h *MinIntHeap) Pop() interface{} {
 	return x
 }
 
-type MaxIntHeap []int
-
-func (h MaxIntHeap) Len() int           { return len(h) }
-func (h MaxIntHeap) Less(i, j int) bool { return h[i] > h[j] } // min-heap
-func (h MaxIntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *MaxIntHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
-}
-
-func (h *MaxIntHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[:n-1]
-	return x
-}
-
-type cpu {
-	min M
-}
-
 func minimumMachines(tasks [][]int) int {
+	n := len(tasks)
+	if n == 0 {
+		return 0
+	}
 
-	minHeap := make(MinIntHeap, 0)
-	maxHeap := make(MaxIntHeap, 0)
+	sort.Slice(tasks, func(i, j int) bool {
+		return tasks[i][0] < tasks[j][0]
+	})
 
+	h := &MinIntHeap{tasks[0][1]}
+	heap.Init(h)
 
-	cpus := make([])
+	maxCPUs, counter := 1, 1
+	for i := 1; i < n; i++ {
+		if tasks[i][0] < (*h)[0] {
+			heap.Push(h, tasks[i][1])
+			counter++
+			if counter > maxCPUs {
+				maxCPUs = counter
+			}
+		} else {
+			heap.Pop(h)
+			heap.Push(h, tasks[i][1])
+		}
+	}
 
-	return -1
+	return maxCPUs
 }
